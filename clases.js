aaa5c4919eea14f2e7b3d9dc6a10eb1c4c1d2bee
GGrop <giuliano.gropponi@gmail.com>
2023-02-07 14:21:01 -0300

    homework class1 part2(monthly salary)

--- clases.js
+++ clases.js
@@ -8,5 +8,15 @@ let anoActual = Number(prompt("Inserte el año actual:"))
 let anoNacimiento = Number(prompt("Inserte tu ano de nacimiento"))
 
 alert("su edad es: " + CalcularEdad(anoActual, anoNacimiento))
-*/
 
+///////////////////////////////////////////////////////
+
+function calcularSalarioMensual(salarioAnual){
+    const mesesEnUnAnio=12
+    return salarioAnual/mesesEnUnAnio
+}
+let salarioAnual = Number(prompt("inserte su salario anual:"))
+
+console.log("su salario mensual es: " + calcularSalarioMensual(salarioAnual))
+
+*/
