bfb9c3da0ce95da23e579acb42310f013537d1bb
GGrop <giuliano.gropponi@gmail.com>
2023-02-07 13:57:46 -0300

    correccion Number() en el prompt/mejora del nombre de la funcion a infinitivo

--- clases.js
+++ clases.js
@@ -1,11 +1,12 @@
 //CLASE 1
 /*
-function CalcularEdad(anoActual, anoNacimiento){
+function CalcularEdadUsuario(anoActual, anoNacimiento){
     return anoActual-anoNacimiento
 }
 
-let anoActual = prompt("Inserte el año actual:")
-let anoNacimiento = prompt("Inserte tu ano de nacimiento")
+let anoActual = Number(prompt("Inserte el año actual:"))
+let anoNacimiento = Number(prompt("Inserte tu ano de nacimiento"))
 
 alert("su edad es: " + CalcularEdad(anoActual, anoNacimiento))
 */
+
