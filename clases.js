acec4bd0c8962054b4e26e17c1eb7b5db61b963e
GGrop <giuliano.gropponi@gmail.com>
2023-02-07 13:49:40 -0300

    HomeWork class1

---
+++ clases.js
@@ -0,0 +1,11 @@
+//CLASE 1
+/*
+function CalcularEdad(anoActual, anoNacimiento){
+    return anoActual-anoNacimiento
+}
+
+let anoActual = prompt("Inserte el año actual:")
+let anoNacimiento = prompt("Inserte tu ano de nacimiento")
+
+alert("su edad es: " + CalcularEdad(anoActual, anoNacimiento))
+*/
